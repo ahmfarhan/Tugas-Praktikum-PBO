2c2578f612695d56890e7e1e58913d7efe06ae21
ahmfarhan <73058791+ahmfarhan@users.noreply.github.com>
2022-04-05 23:17:48 +0700

    Add files via upload

---
+++ TUGAS 2-PRAK PBO/src/Main/MenghitungBangun.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author user
+ */
+public interface MenghitungBangun {
+    double PHI = 3.14;
+    double hitungLuas();
+    double hitungKeliling();
+}
---
+++ TUGAS 2-PRAK PBO/src/Main/Tabung.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author user
+ */
+public class Tabung extends Lingkaran implements MenghitungRuang {
+    private double tinggi, hasilVol, hasilLuasP;
+
+    public Tabung(double r, double t) {
+        super(r);
+        tinggi = t;
+    }
+
+    public double getTinggi() {
+        return tinggi;
+    }
+
+    public void setTinggi(double tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    @Override
+    public double hitungVolume() {
+        return PHI * (super.getJari() * super.getJari()) * tinggi;
+    }
+
+    @Override
+    public double hitungLuasP() {
+        return 2 * (PHI * super.getJari()) * (super.getJari() + tinggi);
+    }
+    
+}
